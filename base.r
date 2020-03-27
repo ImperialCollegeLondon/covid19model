205ef75a9bb64ba0130ba8de87e5d329bcab3a4b
Swapnil Mishra <swapnilmishra.anu@gmail.com>
2020-03-27 08:02:49 +0000

    r code to match the paper runs

--- base.r
+++ base.r
@@ -196,7 +196,7 @@ m = stan_model(paste0('stan-models/',StanModel,'.stan'))
 if(DEBUG) {
   fit = sampling(m,data=stan_data,iter=40,warmup=20,chains=2)
 } else { 
-  fit = sampling(m,data=stan_data,iter=4000,warmup=2000,chains=8,thin=4,control = list(adapt_delta = 0.90, max_treedepth = 10))
+  fit = sampling(m,data=stan_data,iter=4000,warmup=1000,chains=4,thin=4,control = list(adapt_delta = 0.90, max_treedepth = 10))
 }  
 
 out = rstan::extract(fit)
@@ -231,6 +231,6 @@ colnames(mu) = countries
 g = (mcmc_intervals(mu,prob = .9))
 ggsave(sprintf("figures/%s-%s-covars-mu.png",StanModel,JOBID),g,width=4,height=6)
 
-system(paste0("Rscript plot-3-panel.r",StanModel,'-',JOBID,'-stanfit.Rdata'))
-system(paste0("Rscript plot-forecast.r",StanModel,'-',JOBID,'.Rdata'))
+system(paste0("Rscript plot-3-panel.r ",StanModel,'-',JOBID,'-stanfit.Rdata'))
+system(paste0("Rscript plot-forecast.r ",StanModel,'-',JOBID,'.Rdata'))
 
