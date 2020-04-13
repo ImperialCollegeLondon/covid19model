dd7f4ea603bb64c4bf08bc133acffddf17a892f5
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-04-13 13:15:39 +0200

    Modularisation of dataframe transformation
    With a bit of documentation for help

--- data/extract-reg-opencovid2ICLcsv.py
+++ data/extract-reg-opencovid2ICLcsv.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+Provides a command line program which takes in arguments:
 
+extract-reg-opencovid2ICLcsv.py <opencovid csv file>  <maille_code to extract>
+"""
+
+import sys
 import pandas as pd
 from datetime import datetime
-import sys
+
+data_dir = 'data/'
 
 
 def dt_to_dec(dt):
@@ -15,6 +22,56 @@ def dt_to_dec(dt):
     # year value + seconds so far / seconds in year
     return dt.year + ((dt - year_start).total_seconds() /
                       float((year_end - year_start).total_seconds()))
+
+
+def convert_opencovidfr_to_ICL_model(srcReg, population_region):
+    """
+    FUnction that convertsfrom the tabular headers of opencovid19-fr to those
+    needed by the covid19 model.
+
+    Maps:
+        date, cas_confirmes, deces, maille_nom, maille_code, maille_code, date
+    to:
+        dateRep, cases, deaths, countriesAndTerritories, geoId,
+        countryterritoryCode, t
+
+    and population_region -> popData2018
+
+    :param      srcReg:             The opencovid format data frame for a
+    region.
+    :type       srcReg:             pd.DataFrame()
+    :param      population_region:  The population of the region.
+    :type       population_region:  double
+    """
+    dst = pd.DataFrame()
+    # mapping attributs src -> dst
+
+    dst['dateRep'] = srcReg['date'].values
+    # conversion vers des vraies dates
+    dst['dateRep'] = dst['dateRep'].apply(pd.to_datetime, format="%Y-%m-%d")
+
+    # valeurs bidon pour le moment
+    dst['day'] = 1
+    dst['month'] = 1
+    dst['year'] = 2020
+
+    dst['cases'] = srcReg['cas_confirmes'].values
+
+    # avec ou sans les ehpads ?
+    dst['deaths'] = srcReg['deces'].values
+
+    dst['countriesAndTerritories'] = srcReg['maille_nom'].values
+    dst['geoId'] = srcReg['maille_code'].values
+    dst['countryterritoryCode'] = srcReg['maille_code'].values
+
+    # valeurs 2020
+    dst['popData2018'] = population_region
+
+    dst["t"] = dst["dateRep"].apply(lambda v: dt_to_dec(v))
+
+    # et pour finir on re-sérialise la date sous un autre format
+    dst['dateRep'] = dst['dateRep'].apply(lambda x: x.strftime('%d/%m/%Y'))
+    return dst
 
 
 if len(sys.argv) != 3:
@@ -48,34 +105,8 @@ print(reg + " : " + str(srcReg.shape[0]) + " lignes")
 # ATTN ne fonctionne que si on est MONO région
 srcReg = srcReg.drop_duplicates(subset=["date"])
 
-dst = pd.DataFrame()
-# mapping attributs src -> dst
 
-dst['dateRep'] = srcReg['date'].values
-# conversion vers des vraies dates
-dst['dateRep'] = dst['dateRep'].apply(pd.to_datetime, format="%Y-%m-%d")
-
-# valeurs bidon pour le moment
-dst['day'] = 1
-dst['month'] = 1
-dst['year'] = 2020
-
-dst['cases'] = srcReg['cas_confirmes'].values
-
-# avec ou sans les ehpads ?
-dst['deaths'] = srcReg['deces'].values
-
-dst['countriesAndTerritories'] = srcReg['maille_nom'].values
-dst['geoId'] = srcReg['maille_code'].values
-dst['countryterritoryCode'] = srcReg['maille_code'].values
-
-# valeurs 2020
-dst['popData2018'] = popreg[reg]
-
-dst["t"] = dst["dateRep"].apply(lambda v: dt_to_dec(v))
-
-# et pour finir on re-sérialise la date sous un autre format
-dst['dateRep'] = dst['dateRep'].apply(lambda x: x.strftime('%d/%m/%Y'))
+dst = convert_opencovidfr_to_ICL_model(srcReg, popreg[reg])
 
 # et voilà
 dst.to_csv(reg,index=False)
