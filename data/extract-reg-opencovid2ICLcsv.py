7090c1d63cc6af27008b14b129abe4e64ddfaa73
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-04-13 11:57:14 +0200

    Formatting tweaks to data conversion

--- data/extract-reg-opencovid2ICLcsv.py
+++ data/extract-reg-opencovid2ICLcsv.py
@@ -4,33 +4,44 @@ import pandas as pd
 from datetime import datetime
 import sys
 
+
+def dt_to_dec(dt):
+    """Convert a datetime to decimal year.
+    Thanks to https://github.com/ImperialCollegeLondon/covid19model/blob/
+    master/Python/src/util.py
+    """
+    year_start = datetime(dt.year, 1, 1)
+    year_end = year_start.replace(year=dt.year+1)
+    # year value + seconds so far / seconds in year
+    return dt.year + ((dt - year_start).total_seconds() /
+                      float((year_end - year_start).total_seconds()))
+
+
 if len(sys.argv) != 3:
-    print("Usage : extract-reg-opencovid2ICLcsv.py <opencovid csv file> <maille_code to extract>")
-    print("   Will read opencovid csv file, extract maille_code data and output them in maille_code.csv with columns needed by base.r")
+    print(
+        "Usage : extract-reg-opencovid2ICLcsv.py <opencovid csv file>  "
+        + " <maille_code to extract>")
+    print(
+        "   Will read opencovid csv file, extract maille_code data and output"
+        + " them in maille_code.csv with columns needed by base.r")
     sys.exit(1)
-    
+
 fic = sys.argv[1]
 reg = sys.argv[2]
 
 # TODO…
-popreg = { "REG-93" : 5059473 }
-
-def dt_to_dec(dt):
-    """Convert a datetime to decimal year.
-    Thanks to https://github.com/ImperialCollegeLondon/covid19model/blob/master/Python/src/util.py
-    """
-    year_start = datetime(dt.year, 1, 1)
-    year_end = year_start.replace(year=dt.year+1)
-    return dt.year + ((dt - year_start).total_seconds() /  # seconds so far         
-                      float((year_end - year_start).total_seconds())) # seconds in year
+popreg = {
+    "REG-93": 5059473,
+}
 
 src = pd.read_csv(fic)
 
 # filtrage sur maille_code
-srcReg = src.loc[lambda df: df.maille_code == reg,:]
+srcReg = src.loc[lambda df: df.maille_code == reg, :]
 
-# notre intérêt est sur la colonne décès, on supprime donc les lignes où cette valeur n'est pas connue
-srcReg = srcReg.dropna(how='all',subset=["deces"])
+# notre intérêt est sur la colonne décès, on supprime donc les lignes où cette
+# valeur n'est pas connue
+srcReg = srcReg.dropna(how='all', subset=["deces"])
 print(reg + " : " + str(srcReg.shape[0]) + " lignes")
 
 # Attn il y a des doublons - on les élimines, celui qui reste est indéterminé
