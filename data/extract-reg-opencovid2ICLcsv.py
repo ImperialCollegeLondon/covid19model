3b14bb7335440340f092cbefbfa070281b01c362
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-04-13 13:23:38 +0200

    Functionalise the command line

--- data/extract-reg-opencovid2ICLcsv.py
+++ data/extract-reg-opencovid2ICLcsv.py
@@ -9,7 +9,11 @@ import sys
 import pandas as pd
 from datetime import datetime
 
+# Module wide variables
 data_dir = 'data/'
+popreg = {
+    "REG-93": 5059473,
+}
 
 
 def dt_to_dec(dt):
@@ -74,40 +78,43 @@ def convert_opencovidfr_to_ICL_model(srcReg, population_region):
     return dst
 
 
-if len(sys.argv) != 3:
-    print(
-        "Usage : extract-reg-opencovid2ICLcsv.py <opencovid csv file>  "
-        + " <maille_code to extract>")
-    print(
-        "   Will read opencovid csv file, extract maille_code data and output"
-        + " them in maille_code.csv with columns needed by base.r")
-    sys.exit(1)
+def process_from_cmd():
 
-fic = sys.argv[1]
-reg = sys.argv[2]
+    if len(sys.argv) != 3:
+        print(
+            "Usage : extract-reg-opencovid2ICLcsv.py <opencovid csv file>  "
+            + " <maille_code to extract>")
+        print(
+            "   Will read opencovid csv file, extract maille_code data and output"
+            + " them in maille_code.csv with columns needed by base.r")
+        sys.exit(1)
 
-# TODO get population data for all regions and departments
-# look at https://github.com/scrouzet/covid19-incrementality
-popreg = {
-    "REG-93": 5059473,
-}
+    fic = sys.argv[1]
+    reg = sys.argv[2]
 
-src = pd.read_csv(fic)
+    # TODO get population data for all regions and departments
+    # look at https://github.com/scrouzet/covid19-incrementality
 
-# filtrage sur maille_code
-srcReg = src.loc[lambda df: df.maille_code == reg, :]
+    src = pd.read_csv(fic)
 
-# notre intérêt est sur la colonne décès, on supprime donc les lignes où cette
-# valeur n'est pas connue
-srcReg = srcReg.dropna(how='all', subset=["deces"])
-print(reg + " : " + str(srcReg.shape[0]) + " lignes")
-# Nous avons besoins de remplir les cas_confirmes manquant avec des 0
-srcReg["cas_confirmes"] = srcReg["cas_confirmes"].fillna(0)
+    # filtrage sur maille_code
+    srcReg = src.loc[lambda df: df.maille_code == reg, :]
 
-# Attn il y a des doublons - on les élimines, celui qui reste est indéterminé
-srcReg = srcReg.drop_duplicates(subset=["date", "maille_code"])
+    # notre intérêt est sur la colonne décès, on supprime donc les lignes où cette
+    # valeur n'est pas connue
+    srcReg = srcReg.dropna(how='all', subset=["deces"])
+    print(reg + " : " + str(srcReg.shape[0]) + " lignes")
+    # Nous avons besoins de remplir les cas_confirmes manquant avec des 0
+    srcReg["cas_confirmes"] = srcReg["cas_confirmes"].fillna(0)
 
-dst = convert_opencovidfr_to_ICL_model(srcReg, popreg[reg])
+    # Attn il y a des doublons - on les élimines, celui qui reste est indéterminé
+    srcReg = srcReg.drop_duplicates(subset=["date", "maille_code"])
 
-# et voilà
-dst.to_csv(data_dir + reg + '.csv', index=False)
+    dst = convert_opencovidfr_to_ICL_model(srcReg, popreg[reg])
+
+    # et voilà
+    dst.to_csv(data_dir + reg + '.csv', index=False)
+
+
+if __name__ == "__main__":
+    process_from_cmd()
