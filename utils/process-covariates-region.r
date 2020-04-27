502c10192b9db3f6873b6c143a7a268f62cef5ae
Alexandre Payot <18074599+payoto@users.noreply.github.com>
2020-04-27 18:07:20 +0200

    Merge opencovid and ICL v3 code

--- utils/process-covariates-region.r
+++ utils/process-covariates-region.r
@@ -22,6 +22,7 @@ process_covariates_region <- function(region_to_country_map, interventions,
     )
     serial.interval = rbind(serial.interval, pad_serial.interval)
   }
+  death_thresh_epi_start <- 10
   # various distributions required for modeling
   infection_to_onset <- c("mean"=5.1, "deviation"=0.86)
   onset_to_death <- c("mean"=18.8, "deviation"=0.45)
@@ -50,37 +51,42 @@ process_covariates_region <- function(region_to_country_map, interventions,
   {
     Country = region_to_country_map[[Region]]
     print(sprintf("Region: %s in country: %s ",Region,Country))
-    IFR=ifr.by.country$ifr[ifr.by.country$country == Country]
-    
-    covariates1 <- covariates[covariates$Country == Country, c(2,3,4,5,6)]
-    
-    d1_pop = ifr.by.country[ifr.by.country$country==Country,]
-    d1=d[d$Countries.and.territories==Region,c(1,5,6,7)]
-    d1$date = as.Date(d1$DateRep,format='%d/%m/%Y')
-    d1$t = decimal_date(d1$date) 
-    d1=d1[order(d1$t),]
-    if(length(d1$date) == 0){
+    if(any(ifr.by.country$country == Region)){
+        # to add
+      IFR <- ifr.by.country$ifr[ifr.by.country$country == Country]
+      region_pop <- ifr.by.country[ifr.by.country$country==Country,]
+    } else {
+        IFR <- ifr.by.country$ifr[ifr.by.country$country == Country]
+        region_pop <- ifr.by.country[ifr.by.country$country==Country,]
+
+    }
+
+    if(!any(d$Country==Region)){
       preprocess_error = TRUE
       message(sprintf(
-        "ERROR: Region %s in country %s had no data (d1 length(d1)==0)", Region, Country))
+        "ERROR: Region %s in country %s had no data (region length(region)==0)", Region, Country))
       next
     }
+    region <- d[d$Country==Region,]
+    region$DateRep <-region$DateRep
+    region <-region[order(as.Date(region$DateRep)),]  # ensure date ordering
+    
+    # padding in raw data backwards ex. portugal
     date_min <- dmy('31/12/2019') 
-    if (as.Date(d1$DateRep[1], format='%d/%m/%Y') > as.Date(date_min, format='%d/%m/%Y')){
-      pad_days <- as.Date(d1$DateRep[1], format='%d/%m/%Y') - date_min
+    if (region$DateRep[1] > date_min){
+      print(paste(Region,'In padding death data'))
+      pad_days <-region$DateRep[1] - date_min
       pad_dates <- date_min + days(1:pad_days[[1]]-1)
-      padded_data <- data.frame("Countries.and.territories" = rep(Region, pad_days),
-                                "DateRep" = format(pad_dates, '%d/%m/%Y'),
-                                "t" = decimal_date(as.Date(pad_dates,format='%d/%m/%Y')),
-                                "date" = as.Date(pad_dates,format='%d/%m/%Y'),
+      padded_data <- data.frame("Country" = rep(Region, pad_days),
+                                "DateRep" = pad_dates,
                                 "Cases" = as.integer(rep(0, pad_days)),
                                 "Deaths" = as.integer(rep(0, pad_days)),
                                 stringsAsFactors=F)
       
-      d1 <- bind_rows(padded_data, d1)
+     region <- bind_rows(padded_data,region)
     }
-    index = which(d1$Cases>0)[1]
-    index1 = which(cumsum(d1$Deaths)>=death_thresh_epi_start)[1] # also 5
+    index = which(region$Cases>0)[1]
+    index1 = which(cumsum(region$Deaths)>=death_thresh_epi_start)[1] # also 5
     if (is.na(index1)) {
       preprocess_error = TRUE
       message(sprintf(
@@ -89,57 +95,54 @@ process_covariates_region <- function(region_to_country_map, interventions,
       next
     }
     index2 = index1-30
-   
+    
     print(sprintf("First non-zero cases is on day %d, and 30 days before 10 deaths is day %d",index,index2))
-    d1=d1[index2:nrow(d1),]
+    region=region[index2:nrow(region),]
     stan_data$EpidemicStart = c(stan_data$EpidemicStart,index1+1-index2)
-    stan_data$pop = c(stan_data$pop, d1_pop$popt)
-    
-    
-    for (ii in 1:ncol(covariates1)) {
-      covariate = names(covariates1)[ii]
-      d1[covariate] <- (as.Date(d1$DateRep, format='%d/%m/%Y') >= as.Date(covariates1[1,covariate]))*1  # should this be > or >=?
+    stan_data$pop = c(stan_data$pop,region_pop$popt)
+    # NPI interventionss are being used
+    interventions_region <- interventions[interventions$Country == Country, c(2,3,4,5,6)] # school, self-isolation, public, lockdown, social-distancing
+    for (ii in 1:ncol(interventions_region)) {
+      covariate = names(interventions_region)[ii]
+      region[covariate] <- (region$DateRep >= interventions_region[1,covariate])*1  # should this be > or >=?
     }
     
-    dates[[Region]] = d1$date
+    dates[[Region]] =region$DateRep
     # hazard estimation
-    N = length(d1$Cases)
+    N = length(region$Cases)
     print(sprintf("%s has %d days of data",Region,N))
     forecast = N2 - N
     if(forecast < 0) {
-      print(sprintf("%s: %d", Country, N))
-      print("ERROR!!!! increasing N2 to have at least 7 days")
+      print(sprintf("%s: %d", Region, N))
+      print("ERROR!!!! increasing N2")
       N2 = N
       forecast = N2 - N
     }
     
     # IFR is the overall probability of dying given infection
     convolution = function(u) (IFR * ecdf.saved(u))
-
+    
     f = rep(0,N2) # f is the probability of dying on day i given infection
     f[1] = (convolution(1.5) - convolution(0))
     for(i in 2:N2) {
       f[i] = (convolution(i+.5) - convolution(i-.5)) 
     }
-    
-    reported_cases[[Region]] = as.vector(as.numeric(d1$Cases))
-    deaths=c(as.vector(as.numeric(d1$Deaths)),rep(-1,forecast))
-    cases=c(as.vector(as.numeric(d1$Cases)),rep(-1,forecast))
-    deaths_by_country[[Region]] = as.vector(as.numeric(d1$Deaths))
-    covariates2 <- as.data.frame(d1[, colnames(covariates1)])
-    # x=1:(N+forecast)
-    covariates2[N:(N+forecast),] <- covariates2[N,]
-    
+    reported_cases[[Region]] = as.vector(as.numeric(region$Cases))
+    deaths=c(as.vector(as.numeric(region$Deaths)),rep(-1,forecast))
+    cases=c(as.vector(as.numeric(region$Cases)),rep(-1,forecast))
+    deaths_by_country[[Region]] = as.vector(as.numeric(region$Deaths))
+    region_intervention <- as.data.frame(region[, colnames(interventions_region)])
+    # This line will prevent modelling of a future change of interventions
+    region_intervention[N:(N+forecast),] <- region_intervention[N,]
+    school =region_intervention[,1]
+    selfIsolation =region_intervention[,2]
+    publicEvents = region_intervention[,3]
+    lockdown = region_intervention[,4]
+    socialDistancing = region_intervention[,5]
+    firstIntervention = 1*((school+ selfIsolation+ publicEvents+ lockdown + socialDistancing) >= 1)
     ## append data
     stan_data$N = c(stan_data$N,N)
     # stan_data$x = cbind(stan_data$x,x)
-    stan_data$covariate1 = cbind(stan_data$covariate1,covariates2[,1])
-    stan_data$covariate2 = cbind(stan_data$covariate2,covariates2[,2])
-    stan_data$covariate3 = cbind(stan_data$covariate3,covariates2[,3])
-    # stan_data$covariate4 is an "any" covariates marker calculated below
-    stan_data$covariate4 = cbind(stan_data$covariate4,covariates2[,4])
-    stan_data$covariate5 = cbind(stan_data$covariate5,covariates2[,4])
-    stan_data$covariate6 = cbind(stan_data$covariate6,covariates2[,5])
     stan_data$f = cbind(stan_data$f,f)
     stan_data$deaths = cbind(stan_data$deaths,deaths)
     stan_data$cases = cbind(stan_data$cases,cases)
@@ -149,24 +152,16 @@ process_covariates_region <- function(region_to_country_map, interventions,
     if(length(stan_data$N) == 1) {
       stan_data$N = as.array(stan_data$N)
     }
+    df_features = data.frame('school' = school, 'selfIsolation' = selfIsolation, 'publicEvents' = publicEvents,
+                             'firstIntervention' = firstIntervention, 'lockdown' = lockdown, 'socialDistancing' = socialDistancing)
+    features <- as.matrix(df_features)
+    covariate_list[[k]] <- features
+    k <- k+1
   }
-
-  if(preprocess_error){
-    stop(sprintf(
-        "ERROR: There were errors during preprocessing, check for error messages."))
+  stan_data$P = dim(features)[2]
+  stan_data$X = array(NA, dim = c(stan_data$M , stan_data$N2 ,stan_data$P ))
+  for (i in 1:stan_data$M){
+    stan_data$X[i,,] = covariate_list[[i]] 
   }
-
-  # create the `any intervention` covariate
-  stan_data$covariate4 = 1*as.data.frame((stan_data$covariate1+
-                                            stan_data$covariate2+
-                                            stan_data$covariate3+
-                                            stan_data$covariate5+
-                                            stan_data$covariate6) >= 1)
-
-  return(list(
-    "stan_data" = stan_data, 
-    "dates" = dates, 
-    "reported_cases"=reported_cases,
-    "deaths_by_country" = deaths_by_country
-  ))
+  return(list("stan_data" = stan_data, "dates" = dates, "reported_cases"=reported_cases, "deaths_by_country" = deaths_by_country))
 }
