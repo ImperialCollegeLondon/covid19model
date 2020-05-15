c1c86a1282bef9dc11de78f6cbb209f5c14f8f4b
Swapnil Mishra <swapnilmishra.anu@gmail.com>
2020-05-15 03:00:32 +0100

    As new ECDC data had negative deaths stopping it

--- web-fetch-and-run.r
+++ web-fetch-and-run.r
@@ -1,8 +1,8 @@
-print("Fetching data from ECDC")
-fetch_ecdc_error <- system("Rscript data/fetch-ecdc.r",intern=FALSE)
-if(fetch_ecdc_error != 0){
-  stop(sprintf("Error while fetching data from the ECDC! Code: %d", fetch_ecdc_error))
-}
+# print("Fetching data from ECDC")
+# fetch_ecdc_error <- system("Rscript data/fetch-ecdc.r",intern=FALSE)
+# if(fetch_ecdc_error != 0){
+#   stop(sprintf("Error while fetching data from the ECDC! Code: %d", fetch_ecdc_error))
+# }
 
 run_base_error <- system("Rscript base.r base",intern=FALSE)
 if(run_base_error != 0){
